07bdc2d247ddcab55f45d325dcbf98339c8aa193
nikitmeena02-max <nikitmeena02@gmail.com>
2026-02-20 01:09:48 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
-import { supabase } from "./supabaseClient";
-import React from "https://esm.sh/react@18";
-import { createRoot } from "https://esm.sh/react-dom@18/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "./index.css";
+import { supabase } from "./supabaseClient";
 
-createRoot(document.getElementById("root")).render(
-  React.createElement(App)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
