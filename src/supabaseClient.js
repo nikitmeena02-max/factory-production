a0c4115191ea14c075ab44a2425ff8bd5a68f1ac
nikitmeena02-max <nikitmeena02@gmail.com>
2026-02-20 01:39:08 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const supabaseUrl = "https://qigcozewxkcclcxtxkmm.supabase.co"
-const supabaseKey = "sb_publishable_XF2xdJQwVE6X_R56iRfTAg_h8IiBP-H"
+const supabaseUrl = "https://qigcozewxkcclcxtxkmm.supabase.co";
+const supabaseKey = "sb_publishable_XF2xdJQwVE6X_R56iRfTAg_h8IiBP-H";
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
