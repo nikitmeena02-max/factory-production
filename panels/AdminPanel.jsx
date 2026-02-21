880a32b58106b1099fc8ae3bf023d7f4efc28ba8
nikitmeena02-max <nikitmeena02@gmail.com>
2026-02-21 22:05:48 +0530

    Create AdminPanel.jsx

---
+++ panels/AdminPanel.jsx
@@ -0,0 +1,10 @@
+export default function AdminPanel({ user, onLogout }) {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Welcome {user.name}</h2>
+      <h3>Role: {user.role}</h3>
+
+      <button onClick={onLogout}>Logout</button>
+    </div>
+  );
+}
